98771a501f36ffbf61c8b474c78dc8c3c87643ef
i110156 <i110156@PCI085.dei.isep.ipp.pt>
2013-03-08 14:46:30 +0000

    olá

--- teste/src/teste/Teste.java
+++ teste/src/teste/Teste.java
@@ -6,7 +6,7 @@ public class Teste {
 
     public static void main(String[] args) {
        
-        System.out.println("Olá");
+    System.out.println("Olá");
     System.out.println("fork");
     }
 }
