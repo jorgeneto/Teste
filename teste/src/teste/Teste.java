7955a5a26b8af58adacfa5c3e3be017425c494b2
Duarte Teles <duarte.teles.2@gmail.com>
2013-03-08 15:11:43 +0000

    fiz nada

--- teste/src/teste/Teste.java
+++ teste/src/teste/Teste.java
@@ -1,16 +1,14 @@
-
 package teste;
-
 
 public class Teste {
 
-    public static void main(String[] args) {
-       
+	public static void main(String[] args) {
 
-    System.out.println("WHOHAWHOLAWHOA");
-    System.out.println("fork");
-    System.out.println("fork WHOWHOWHOA");
-    System.out.println("hahahahahaha");
+		System.out.println("blah");
+		System.out.println("WHOHAWHOLAWHOA");
+		System.out.println("fork");
+		System.out.println("fork WHOWHOWHOA");
+		System.out.println("hahahahahaha");
 
-    }
+	}
 }
